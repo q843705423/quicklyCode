1d4314664c2b619e343d8b8cd8067cc7477a6c22
qiqi.chen <qiqi.chen@teradata.com>
2019-05-06 22:35:26 +0800

    resolve read-Only error

--- src/com/teradata/action/QuicklyCode.java
+++ src/com/teradata/action/QuicklyCode.java
@@ -2,21 +2,26 @@ package com.teradata.action;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
-
 import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.ui.Messages;
 import com.teradata.document.DocumentWrite;
 import com.teradata.ui.QuestionDialog;
+
+import java.util.Objects;
 
 
 public class QuicklyCode extends AnAction {
 
     private DocumentWrite documentWrite;
 
+    private final String QUICKLY_CODE = "QuicklyCode";
+
     @Override
     public void actionPerformed(AnActionEvent e) {
-
 
 
         QuestionDialog questionDialog = new QuestionDialog();
@@ -28,10 +33,18 @@ public class QuicklyCode extends AnAction {
         int start = questionDialog.start;
         int end = questionDialog.end;
         String target = questionDialog.target;
-//        Application application = ApplicationManager.getApplication();
-        CommandProcessor.getInstance().runUndoTransparentAction(()->{
+        CommandProcessor.getInstance().runUndoTransparentAction(() -> {
             String content = documentWrite.get();
-            documentWrite.set(content.substring(0,start)+target+content.substring(end));
+            if (content != null && content.length() != 0) {
+                Application app = ApplicationManager.getApplication();
+                app.runWriteAction(() -> {
+                    Document document = Objects.requireNonNull(e.getData(PlatformDataKeys.EDITOR)).getDocument();
+                    document.setReadOnly(false);
+                    document.setText(content.substring(0, start) + target + content.substring(end));
+                });
+            } else {
+                Messages.showErrorDialog("you don't choose any code", QUICKLY_CODE);
+            }
         });
     }
 
