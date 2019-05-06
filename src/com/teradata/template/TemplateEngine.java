60cea5306ed47f99375fb1bd04353463e49b2fdb
qiqi.chen <qiqi.chen@teradata.com>
2019-05-06 23:01:42 +0800

    add new var

--- src/com/teradata/template/TemplateEngine.java
+++ src/com/teradata/template/TemplateEngine.java
@@ -17,10 +17,10 @@ public class TemplateEngine {
             String temp = text;
             while (temp.contains("$")) {
                 int first = temp.indexOf("$");
-                int next = temp.indexOf("$", first+1);
-                String anliayzed = temp.substring(first+1, next );
+                int next = temp.indexOf("$", first + 1);
+                String anliayzed = temp.substring(first + 1, next);
                 String result = change(anliayzed, fieldList.get(i));
-                temp = temp.replace("$"+anliayzed+"$", result);
+                temp = temp.replace("$" + anliayzed + "$", result);
             }
             texts.append(temp);
         }
@@ -28,9 +28,9 @@ public class TemplateEngine {
     }
 
 
-    public static void  main(String...args) throws Exception {
-        HashMap<String,Object>map = new HashMap<>();
-        List<FieldInfo>list = new ArrayList<>();
+    public static void main(String... args) throws Exception {
+        HashMap<String, Object> map = new HashMap<>();
+        List<FieldInfo> list = new ArrayList<>();
         FieldInfo fieldInfo = new FieldInfo();
         fieldInfo.setFieldName("user_id");
         fieldInfo.setCommit("用户Id");
@@ -39,40 +39,47 @@ public class TemplateEngine {
         fieldInfo1.setFieldName("username");
         fieldInfo1.setCommit("用户名");
         list.add(fieldInfo1);
-        map.put("field",list);
-        String con = render("<v-template field='$field.name$' >$field.comment$<v-template>",map);
+        map.put("field", list);
+        String con = render("<v-template field='$field.name$' >$field.comment$<v-template>", map);
     }
+
     public static String change(String text, FieldInfo fieldInfo) throws Exception {
-        String str[] = text.split("\\.");
-        if (str.length != 2) {
-            throw new Exception("template Exception, analysis ',' anomaly");
-        }
-        if (!str[0].equals("field")) {
-            return text;
-        }
-        if (str[1].equals("name")) {
+//        String str[] = text.split("\\.");
+//        if (str.length > 2) {
+//            throw new Exception("template Exception, analysis ',' anomaly");
+//        }
+//        if (!str[0].equals("field")) {
+//            return text;
+//        }
+
+        if (text.equals("name")) {
             String t = fieldInfo.getFieldName();
             String humpString = DB.underscore2hump(t);
             return humpString;
-        } else if (str[1].equals("comment")) {
+        } else if (text.equals("comment")) {
             return fieldInfo.getCommit();
-        } else if (str[1].equals("default")) {
+        } else if (text.equals("default")) {
             return fieldInfo.getDefultValue();
-        } else if (str[1].equals("isKey")) {
+        } else if (text.equals("isKey")) {
             return fieldInfo.getIsKey();
-        } else if (str[1].equals("canNull")) {
+        } else if (text.equals("canNull")) {
             return fieldInfo.getCanNull();
-        }else if(str[1].equals("Name")){
+        } else if (text.equals("Name")) {
             String humpString = DB.underscore2hump(fieldInfo.getFieldName());
-            return humpString.substring(0,1).toUpperCase()+humpString.substring(1);
-        } else if (str[1].equals("fieldName")) {
+            return humpString.substring(0, 1).toUpperCase() + humpString.substring(1);
+        } else if (text.equals("fieldName")) {
             return fieldInfo.getFieldName();
-        }else if(str[1].equals("type")){
+        } else if (text.equals("type")) {
             return fieldInfo.getType();
-        }else if(str[1].equals("class")){
+        } else if (text.equals("class")) {
             return DB.type2Class(fieldInfo.getType());
+        } else if (text.equals("NAME")) {
+            String humpString = DB.underscore2hump(fieldInfo.getFieldName());
+            return humpString.toUpperCase();
+        } else if (text.equals("FIELD_NAME")) {
+            return fieldInfo.getFieldName().toUpperCase();
         }
-        return "###it is a error field###";
+        return "?" + text + "?";
     }
 
 
